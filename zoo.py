e3f6a379cac2f6efa0768b8325c14ca5ad9fa1a2
buffard <samuelwebber19@gmail.com>
2019-01-11 12:50:48 -0600

    base boiler plate

---
+++ zoo.py
@@ -0,0 +1,7 @@
+# 1. Create a tuple named zoo that contains your favorite animals.
+
+# 2. Find one of your animals using the .index(value) method on the tuple.
+
+# 3. Determine if an animal is in your tuple by using value in tuple.
+
+# 4. Create a variable for each of the animals in your tuple with this cool feature of Python.
